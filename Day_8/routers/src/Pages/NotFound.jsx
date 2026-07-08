c01bc7ce20c447b593d0496811997052e324a259
kamaltamil <kamaltamil0284@gmail.com>
2026-07-08 17:14:26 +0530

    Day_8 Page NotFound updated

--- Day_8/routers/src/Pages/NotFound.jsx
+++ Day_8/routers/src/Pages/NotFound.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { Result } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import CustomButton from '../Components/CustomButton/CustomButton';
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+  const navigate = useNavigate();
+  const token = localStorage.getItem('sessionToken');
 
-export default NotFound
+  const handleGoHome = () => {
+    // If logged in, send them back to the workspace. If not, send them to login.
+    if (token) {
+      navigate('/dashboard/tasks');
+    } else {
+      navigate('/login');
+    }
+  };
+
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      minHeight: '80vh', 
+      width: '100%' 
+    }}>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <CustomButton variant="primary" size="large" onClick={handleGoHome}>
+            Back to Safe Zone
+          </CustomButton>
+        }
+      />
+    </div>
+  );
+};
+
+export default NotFound;
