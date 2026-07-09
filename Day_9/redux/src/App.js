cc841e2fa9982efc1e19e52894f1e3ae79ba2300
kamaltamil <kamaltamil0284@gmail.com>
2026-07-09 11:44:55 +0530

    Day_9 task initialize

---
+++ Day_9/redux/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Typography} from 'antd';
+
+const {Title , Text} = Typography
+
+function App() {
+  return (
+    <>
+      <Title level={1}>Redux toolkit</Title>
+      <Text>Welcome</Text>
+    </>
+  );
+}
+
+export default App;
