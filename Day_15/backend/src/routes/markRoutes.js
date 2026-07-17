76bfeaf5e0c1acc0cd8d40c4a8e0af887c27eead
kamaltamil <kamaltamil0284@gmail.com>
2026-07-17 15:57:43 +0530

    delete

--- Day_15/backend/src/routes/markRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const studentController = require('../controllers/studentController');
-
-router.post('/', studentController.addMark);
-
-module.exports = router;
