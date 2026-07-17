495e170f1a0f8ec586e5a2a02a7917d5818e9fa9
kamaltamil <kamaltamil0284@gmail.com>
2026-07-17 14:25:15 +0530

    Day_15

--- Day_15/frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TaskManager from './components/TaskManager';
-import 'antd/dist/reset.css';
-
-function App() {
-  return <TaskManager />;
-}
-
-export default App;
