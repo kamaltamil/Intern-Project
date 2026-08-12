73ed484c6934a6d27ad0b09ff5171acd90cd32c9
Kamal Tamilselvan <131656397+kamaltamil@users.noreply.github.com>
2026-08-12 15:50:06 +0530

    Protect room update route

--- Day_26/backend/routes/api/v1/rooms.js
+++ Day_26/backend/routes/api/v1/rooms.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const {
   createRoom,
   getAllRooms,
+  updateRoom,
   deleteRoom,
 } = require("../../../controllers/roomsController");
 
 const { requirePermission } = require("../../../middleware/permissionMiddleware");
 
 router.get("/", requirePermission("rooms", "view"), getAllRooms);
-
 router.post("/new", requirePermission("rooms", "create"), createRoom);
-
+router.patch("/:id", requirePermission("rooms", "update"), updateRoom);
 router.delete("/:id", requirePermission("rooms", "delete"), deleteRoom);
 
 module.exports = router;
