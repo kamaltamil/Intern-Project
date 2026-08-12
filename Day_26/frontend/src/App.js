d151b4b2ee0def48224aaaa3146b2f2c2ae652a8
Kamal Tamilselvan <131656397+kamaltamil@users.noreply.github.com>
2026-08-12 14:22:25 +0530

    Sync latest user permissions

--- Day_26/frontend/src/App.js
+++ Day_26/frontend/src/App.js
@@ -1,7 +1,13 @@
 import AppRoutes from './routes/AppRoutes';
+import PermissionSync from './components/PermissionSync';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <PermissionSync />
+      <AppRoutes />
+    </>
+  );
 }
 
 export default App;
