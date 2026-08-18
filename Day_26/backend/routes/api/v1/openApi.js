b8f245c305346af0fdd89ccd3e3d7cb2b0d9feba
Kamal Tamilselvan <131656397+kamaltamil@users.noreply.github.com>
2026-08-18 10:14:15 +0530

    Apply payload validation to open APIs

--- Day_26/backend/routes/api/v1/openApi.js
+++ Day_26/backend/routes/api/v1/openApi.js
@@ -9,13 +9,15 @@ const {
 } = require("../../../controllers/authController");
 
 const { subscribe } = require("../../../controllers/subscriptionController");
-
+const { validateRequest } = require("../../../middleware/validationHandler");
+const { signupPayload, loginPayload } = require("../../../validators/authValidator");
+const { subscriptionPayload } = require("../../../validators/subscriptionValidator");
 const { rateLimiter } = require("../../../config/rateLimiting");
 
-router.post("/signup", register);
-router.post("/login", login);
+router.post("/signup", signupPayload, validateRequest, register);
+router.post("/login", loginPayload, validateRequest, login);
 router.post("/refresh", refresh);
 
-router.use("/subscriptions", rateLimiter, subscribe);
+router.use("/subscriptions", rateLimiter, subscriptionPayload, validateRequest, subscribe);
 
-module.exports = router
+module.exports = router;
